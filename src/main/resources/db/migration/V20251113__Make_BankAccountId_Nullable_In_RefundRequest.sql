68f9c4c0cc0f4584b7a8ca9357780a72b0cdec70
Peaker04 <Kyttde180211@fpt.edu.vn>
2025-11-13 15:19:02 +0700

    chỉnh sửa db

--- src/main/resources/db/migration/V20251113__Make_BankAccountId_Nullable_In_RefundRequest.sql
+++
@@ -1,5 +0,0 @@
--- Make BankAccountId nullable in RefundRequest table
--- Allow RefundRequest to be created without a bank account (admin can add it later)
-
-ALTER TABLE RefundRequest 
-MODIFY COLUMN BankAccountId VARCHAR(36) NULL;
